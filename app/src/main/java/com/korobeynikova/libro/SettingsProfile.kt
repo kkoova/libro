d8fc041332c432a42871f59b835782884b7d72ec
alyak <alya.korobeynikova99@gmail.com>
2024-04-21 12:56:51 +0300

    v2.5

--- app/src/main/java/com/korobeynikova/libro/SettingsProfile.kt
+++ app/src/main/java/com/korobeynikova/libro/SettingsProfile.kt
@@ -5,14 +5,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.database
+import com.google.firebase.database.FirebaseDatabase
 import com.korobeynikova.libro.databinding.FragmentSettingsProfileBinding
 
 class SettingsProfile : Fragment() {
@@ -32,14 +32,37 @@ class SettingsProfile : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         firebaseAuth = FirebaseAuth.getInstance()
-        database = Firebase.database.reference
+        database = FirebaseDatabase.getInstance().reference
 
         val logUotBtn = view.findViewById<ConstraintLayout>(R.id.exitLayout)
         val controller = findNavController()
+        val exit = view.findViewById<ImageView>(R.id.exitImage)
+
+        exit.setOnClickListener { controller.navigate(R.id.startLiginOrSign) }
 
         logUotBtn.setOnClickListener {
             firebaseAuth.signOut()
             Toast.makeText(requireContext(), "Вы вышли из аккаунта", Toast.LENGTH_SHORT).show()
+            val intent = Intent(context, MainLog::class.java)
+            startActivity(intent)
+            MainActivity().finish()
+        }
+
+        val currentUser = firebaseAuth.currentUser
+
+        if (currentUser != null) {
+            val uid = FirebaseAuth.getInstance().currentUser!!.uid
+            database.child("users").child(uid).get()
+                .addOnSuccessListener {
+                    val login = it.child("username").value.toString()
+                    val email = it.child("email").value.toString()
+                    binding.loginText.text = login
+                    binding.emailText.text = email
+                }.addOnFailureListener {
+                    //Toast.makeText(requireContext(), "Данные не были загружены", Toast.LENGTH_SHORT).show()
+                }
+        } else {
+            // Пользователь не вошел в аккаунт
             val intent = Intent(context, MainLog::class.java)
             startActivity(intent)
             MainActivity().finish()
